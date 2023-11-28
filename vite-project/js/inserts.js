7c10b8398a783fbbe230caff8f4b1cb1db697af7
kennethn23 <kennethng1337@gmail.com>
2023-11-28 09:24:11 -0500

    11/28/23

--- vite-project/js/inserts.js
+++ vite-project/js/inserts.js
@@ -1,12 +1,12 @@
 import { DOMSelectors } from './dom';
 
 function insert(array) {
-    array.forEach((item) => DOMSelectors.box.insertAdjacentHTML("beforeend", 
-    `<div class="card" id="all">
+  DOMSelectors.box.innerHTML = "";
+  array.forEach((item) => DOMSelectors.box.insertAdjacentHTML("beforeend", 
+    `<div class="card">
       <h3 class="card-title">${item.name}</h3>
       <img src="${item.image}" alt="${item.name}" class="card-img">
       <h3 class="card-description">${item.price} | ${item.pieces} | ${item.calories} calories</h3>
-      
     </div>
     `)
     );
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,46 +3,51 @@ import { DOMSelectors } from './dom';
 import { menu } from './menu';
 import { insert } from './inserts';
 
-DOMSelectors.all.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
+let buttons = document.querySelectorAll(".food-buttons");
+buttons.forEach((button) => button.addEventListener("click", 
+function () {
+  if (button == button[0]) {
+    insert(menu);
+  };
+  let filter = button.textContent;
+  let filteredMenu = menu.filter((food) => food.type.includes(filter));
+  filteredMenu.forEach((food) => console.log(food));
+  filteredMenu.forEach((food) => insert(food));
+}));
+
+/* DOMSelectors.all.addEventListener("click", function () {
   insert(menu);
 });
 
 DOMSelectors.chicken.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
   const chicken = menu.filter((item) => item.type.includes("chicken"));
   insert(chicken);
 });
 
 DOMSelectors.beef.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
   const beef = menu.filter((item) => item.type.includes("beef"));
   insert(beef);
 });
 
 DOMSelectors.shrimp.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
   const shrimp = menu.filter((item) => item.type.includes("shrimp"));
   insert(shrimp);
 });
 
 DOMSelectors.rice.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
   const rice = menu.filter((item) => item.type.includes("rice"));
   insert(rice);
 });
 
 DOMSelectors.noodle.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
   const noodle = menu.filter((item) => item.type.includes("noodle"));
   insert(noodle);
 });
 
 DOMSelectors.other.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "";
   const other = menu.filter((item) => item.type.includes("other"));
   insert(other);
-});
+}); */
 
 document.querySelector("#theme-button").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
