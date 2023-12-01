5c5882e7c509532d338786fd067d420d311f485e
kennethn23 <kennethng1337@gmail.com>
2023-12-01 09:15:34 -0500

    12/1/23

--- notes.js
+++ notes.js
@@ -12,20 +12,25 @@ aaron.then((result) => {
     console.log(result);
 }); */
 
-
 // REST API
 const url = `https://api.quotable.io/random`;
 
 async function getData(url){
+    let pokemon = "pikachu";
     try {
         const response = await fetch(url);
+        if (response.status != 200) {
+            throw new Error(pokemon);
+            // throw new Error(response.statusText);
+        }
         console.log(response);
         const data = await response.json();
         console.log(data);
         document.querySelector("h1").textContent = data.content;
         document.querySelector("h2").textContent = data.author;
     } catch (error) {
-
+        document.querySelector("h1").textContent = `Sorry, we could not find ${pokemon}`;
+        //document.querySelector("h1").textContent = error;
     }
 }
 getData(url);
